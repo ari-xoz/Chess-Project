f3803547bd2d61b793d85a47bd336baafcd88135
Sami AGOURRAM <agourram.ma@gmail.com>
2022-12-09 00:22:40 +0100

    Interface update + Board class

--- ChessInterface.java
+++ ChessInterface.java
@@ -1,40 +1,59 @@
-package Project;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-import java.awt.*;
-import javax.swing.*;
- 
-public class ChessInterface extends JFrame{
-  JLayeredPane layeredPane;
-  JPanel chessBoard;
-  public ChessInterface(){
-  Dimension boardSize = new Dimension(600, 600);
-  layeredPane = new JLayeredPane();
-  getContentPane().add(layeredPane);
-  layeredPane.setPreferredSize(boardSize);
- 
-  chessBoard = new JPanel();
-  layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
-  chessBoard.setLayout( new GridLayout(8, 8) );
-  chessBoard.setPreferredSize(boardSize);
-  chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
- 
-  for (int i = 0; i < 64; i++) {
-  JPanel square = new JPanel( new BorderLayout() );
-  chessBoard.add( square );
- 
-  int row = (i / 8) % 2;
-  if (row == 0)
-  square.setBackground( i % 2 == 0 ? Color.black : Color.white );
-  else
-  square.setBackground( i % 2 == 0 ? Color.white : Color.black );
-  }
+public class ChessBoard extends Application {
+  private static final int TILE_SIZE = 75;
+  private static final int WIDTH = 8;
+  private static final int HEIGHT = 8;
 
+  public void start(Stage primaryStage) {
+    GridPane board = new GridPane();
+    for (int row = 0; row < HEIGHT; row++) {
+      for (int col = 0; col < WIDTH; col++) {
+        Label tile = new Label();
+        tile.setPrefSize(TILE_SIZE, TILE_SIZE);
+        // Set the background color of the tile based on its position
+        if ((row + col) % 2 == 0) {
+          tile.setStyle("-fx-background-color: #ffe9c5");
+        } else {
+          tile.setStyle("-fx-background-color: #8d816d");
+        }
+        board.add(tile, col, row);
+        
+      }
+    }
+    for (int r = 0; r < HEIGHT; r++) {
+      board.add(new ImageView("Pawn.png"),r,1);
+      board.add(new ImageView("Pawn.png"),r,6);
+    }
+    board.add(new ImageView("king.png"),4,0);
+    board.add(new ImageView("king.png"),4,7);
+    board.add(new ImageView("Rook.png"),0,0);
+    board.add(new ImageView("Rook.png"),7,7);
+    board.add(new ImageView("Rook.png"),7,0);
+    board.add(new ImageView("Rook.png"),0,7);
+    board.add(new ImageView("Knight.png"),1,0);
+    board.add(new ImageView("Knight.png"),6,0);
+    board.add(new ImageView("Knight.png"),1,7);
+    board.add(new ImageView("Knight.png"),6,7);
+    board.add(new ImageView("Queen.png"),3,0);
+    board.add(new ImageView("Queen.png"),3,7);
+    board.add(new ImageView("Bishop.png"),2,0);
+    board.add(new ImageView("Bishop.png"),5,0);
+    board.add(new ImageView("Bishop.png"),2,7);
+    board.add(new ImageView("Bishop.png"),5,7);
+    board.setAlignment(Pos.CENTER);
+    Scene scene = new Scene(board, WIDTH * TILE_SIZE, HEIGHT * TILE_SIZE);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
-  JFrame frame = new ChessInterface();
-  frame.pack();
-  frame.setResizable(false);
-  frame.setVisible(true);
- }
+    launch(args);
+  }
 }
